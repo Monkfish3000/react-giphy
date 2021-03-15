ed1790d7e68dbb20754cb8a029372a16331acf7a
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2021-03-15 21:29:03 +0100

    building gif finder app - css added

--- src/index.jsx
+++ src/index.jsx
@@ -2,30 +2,16 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
+import App from './components/app.jsx';
 
 // const Hello = (props) => {
 //   return <div>Hello, Michael</div>;
 // }
 
-class Hello extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked = false
-    };
-  }
-
-  render() {
-    return (
-      <div>Hello {this.props.name}</div>
-      )
-  }
-}
 
 const root = document.getElementById('root');
 if (root) {
   ReactDOM.render(
-    <Hello name="World" />,
+    <App />
     root);
 }
